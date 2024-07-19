f43151efa3d70d5c530dfa907de081cc59e698e8
Nisrine Hamdaoui <nissrinehm99@gmail.com>
2024-07-20 00:25:15 +0100

    + Repositories

---
+++ backend/Wallet/Wallet/src/main/java/com/wallet/Wallet/Repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.wallet.Wallet.Repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wallet.Wallet.Entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findByFirstNameAndLastName(String firstName, String lastName);
+
+}
